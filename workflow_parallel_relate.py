c8fa275b2d5986eb2b1f570b16eb578b6466a47f
erikfogh <47324240+erikfogh@users.noreply.github.com>
2020-10-19 11:17:20 +0200

    work on making clues able to test many sites

--- workflow_parallel_relate.py
+++ workflow_parallel_relate.py
@@ -11,9 +11,6 @@ genetic_map_x = "/faststorage/home/eriks/relate-clues/data/recombination_maps/ge
 path_to_prepared = "/faststorage/home/eriks/relate-clues/steps/"
 pop_information = {"CEU_kept_prepared/": ["1.25e-8", "6000"],
                    "YRI_kept_prepared/": ["1.25e-8", "15000"]}
-# "CEU_kept_prepared/": ["1.25e-8", "6000"],, "all_individuals_prepared/": ["1.25e-8", "30000"]  # Due to the larger number of samples, jobs fail with out-of-time or memory.
-# for pop_information: key is name of folder in steps, values are mutation rate and effective population size
-# chromosomes = list(range(1, 23))+["X"]
 chromosomes = list(range(1, 23))+["X"]
 l_d = []
 for chrom in chromosomes:
@@ -40,13 +37,12 @@ def full_relate(number, genetic_map, out_dir, pop_inf, prep_dir, relate_path):
     options = {
         "cores": 10,
         "memory": "30g",
-        "walltime": "8:00:00"
+        "walltime": "6:00:00"
     }
     spec = """
     cd {}
     {} --mode All -m {} -N {} --haps {} --sample {} --map {} -o {} --annot {} --dist {} --memory 23
     """.format(out_dir, Relate, m, n, haps, sample, genetic_map, o, annot,  dist)
-    print(spec)
     return AnonymousTarget(inputs=inputs, outputs=outputs, options=options, spec=spec)
 
 
@@ -60,8 +56,8 @@ def estimate_pop_size(number, out_dir, pop_inf, prep_dir, relate_path):
     poplabels = prep_dir+"chrom{}.poplabels".format(number)
     options = {
         "cores": 10,
-        "memory": "30g",
-        "walltime": "20:00:00"
+        "memory": "20g",
+        "walltime": "15:00:00"
     }
     spec = """
     {} -i {} -m {} --poplabels {} -o {} --threshold 0 --num_iter 5
@@ -69,9 +65,9 @@ def estimate_pop_size(number, out_dir, pop_inf, prep_dir, relate_path):
     return AnonymousTarget(inputs=inputs, outputs=outputs, options=options, spec=spec)
 
 
-def detect_selection(number, in_dir, out_results, pop_inf, prep_dir, relate_path):
-    i = in_dir+"chrom{}_popsize.coal".format(number)
-    o = out_results+"chrom{}_selection".format(number)
+def detect_selection(number, out_dir, pop_inf, prep_dir, relate_path):
+    i = out_dir+"chrom{}_popsize.coal".format(number)
+    o = out_dir+"chrom{}_selection".format(number)
     m = pop_inf[0]
     inputs = [i]
     outputs = [o+".freq"]
@@ -87,31 +83,10 @@ def detect_selection(number, in_dir, out_results, pop_inf, prep_dir, relate_path
     return AnonymousTarget(inputs=inputs, outputs=outputs, options=options, spec=spec)
 
 
-# def sample_branch_lengths(number, out_dir, pop_inf, prep_dir, relate_path):
-#     i = out_dir+"chrom{}_popsize.coal".format(number)
-#     o = out_dir+"chrom{}_selection".format(number)
-#     m = pop_inf[0]
-#     inputs = [i]
-#     outputs = [o+".freq"]
-#     Relate = os.path.join(relate_path, "scripts/DetectSelection/DetectSelection.sh")
-#     options = {
-#         "cores": 4,
-#         "memory": "2",
-#         "walltime": "1:00:00"
-#     }
-#     spec = """
-#     {} -i {} -m {} -o {}
-#     """.format(Relate, i[:-4], m, o)
-#     print(spec)
-#     return AnonymousTarget(inputs=inputs, outputs=outputs, options=options, spec=spec)
-
-
 for pop in pop_information:
     pop_name = pop[:-10]
     out_dir = "steps/{}_relate/".format(pop_name)
-    out_results = "results/{}_relate/".format(pop_name)
     os.makedirs(out_dir, exist_ok=True)
-    os.makedirs(out_results, exist_ok=True)
     with Group(gwf, suffix=pop_name) as g:
         relate = g.map(full_relate, l_d, name="relate", extra={
             "out_dir": out_dir, "pop_inf": pop_information[pop],
@@ -121,34 +96,9 @@ for pop in pop_information:
             "out_dir": out_dir, "pop_inf": pop_information[pop],
             "prep_dir": path_to_prepared+pop, "relate_path": path_to_relate
         })
+        print(chromosomes, pop_information[pop], out_dir, path_to_prepared+pop, path_to_relate)
         g.map(detect_selection, chromosomes, name="detect_selection", extra={
-            "in_dir": out_dir, "out_results": out_results, "pop_inf": pop_information[pop],
+            "out_dir": out_dir, "pop_inf": pop_information[pop],
             "prep_dir": path_to_prepared+pop, "relate_path": path_to_relate
         })
-
-#
-# test_input = gwf.target_from_template(
-#     name="CEU_chrom22_test",
-#     template=full_relate(
-#         m="1.25e-8", n="30000", prep_dir="steps/CEU_kept_prepared/", number="22",
-#         genetic_map="data/recombination_maps/genetic_map_chr22_combined_b37.txt",
-#         relate_path=path_to_relate, o="steps/CEU_chrom22_test"
-#     )
-# )
-
-# test_popsize = gwf.target_from_template(
-#     name="CEU_chrom22_popsize",
-#     template=estimate_pop_size(
-#         m="1.25e-8", i=test_input.outputs[0],
-#         prep_dir="steps/CEU_kept_prepared/", number="22",
-#         relate_path=path_to_relate, o="CEU_chrom22_poptest"
-#     )
-# )
-
-# test_detection = gwf.target_from_template(
-#     name="CEU_chrom22_selection",
-#     template=detect_selection(
-#         m="1.25e-8", i=test_input.outputs[0], c=test_popsize.outputs[0],
-#         relate_path=path_to_relate, o="CEU_chrom22_seletest"
-#     )
-# )
+        print(pop)
